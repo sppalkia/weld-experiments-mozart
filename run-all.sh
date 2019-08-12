15389bbc278f56e198305c9e96233c3d92f5c556
Shoumik Palkar <shoumik@cs.stanford.edu>
2019-08-12 03:22:10 +0000

    Fix run script

--- run-all.sh
+++ run-all.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-source ../wedlbench/bin/activate
+set -x
 
-threads=( 1 2 4 8 16 )
+source ~/weld/weldbench/bin/activate
+
+threads=( 16 )
+# threads=( 1 2 4 8 16 )
 runs=${1:-1}
 
 rm -rf results/
@@ -25,16 +28,16 @@ mkdir -p results/shallow_water
 for i in {1..$runs}; do
   for nthreads in "${threads[@]}"; do
     # Birth Analysis
-    python birth_analysis_grizzly.py -t $nthreads -f $HOME/composer/python/benchmarks/datasets/birth_analysis/_data/babynames-xlarge.txt >> results/birth_analysis/weld.stdout 2>> results/birth_analysis/weld.stderr 
+    python birth_analysis_grizzly.py -t $nthreads -f $HOME/composer/python/benchmarks/datasets/birth_analysis/_data/babynames.txt >> results/birth_analysis/weld.stdout 2>> results/birth_analysis/weld.stderr 
     python blackscholes_weld.py -t $nthreads -s 30 >> results/blackscholes/weld.stdout 2>> results/blackscholes/weld.stderr 
-    python crime_index_weld.py -t $nthreads -s 30 >> results/crime_index/weld.stdout 2>> results/crime_index/weld.stderr 
-    python data_cleaning_weld.py -t $nthreads -s 29 >> results/data_cleaning/weld.stdout 2>> results/data_cleaning/weld.stderr 
+    python crime_index_grizzly.py -t $nthreads -s 30 >> results/crime_index/weld.stdout 2>> results/crime_index/weld.stderr 
+    python data_cleaning_grizzly.py -t $nthreads -s 29 >> results/data_cleaning/weld.stdout 2>> results/data_cleaning/weld.stderr 
     python haversine_weld.py -t $nthreads -s 30 >> results/haversine/weld.stdout 2>> results/haversine/weld.stderr 
     python movielens_grizzly.py -t $nthreads -f $HOME/composer/python/benchmarks/datasets/movielens/_data/ml-1m/ >> results/movielens/weld.stdout 2>> results/movielens/weld.stderr 
     # NOTE: These results should be run five times and summed, due to an OOM error in Weld.
     python nbody_weld.py -t $nthreads -s 15 -i 1 >> results/nbody/weld.stdout 2>> results/nbody/weld.stderr 
     # NOTE: These results should be run ten times and summed, due to an OOM error in Weld.
-    python shallow_water_weld.py -t $nthreads -s 15 -i 1 >> results/shallo_water/weld.stdout 2>> results/shallow_water/weld.stderr 
+    python shallow_water_weld.py -t $nthreads -s 15 -i 1 >> results/shallow_water/weld.stdout 2>> results/shallow_water/weld.stderr 
   done
 done
 
