6b6d4eb91012a1eba15802540078896db7b02252
Shoumik Palkar <shoumik@cs.stanford.edu>
2019-08-12 06:39:03 +0000

    Make sure SA_HOME is set

--- run-all.sh
+++ run-all.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+echo "Using SA implementation at ${SA_HOME?"Need to set SA_HOME to run"}"
+
 source ./weldbench/bin/activate
 
 threads=( 16 )
@@ -28,12 +30,12 @@ mkdir -p results/shallow_water
 for i in {1..$runs}; do
   for nthreads in "${threads[@]}"; do
     # Birth Analysis
-    python birth_analysis_grizzly.py -t $nthreads -f $HOME/composer/python/benchmarks/datasets/birth_analysis/_data/babynames.txt >> results/birth_analysis/weld.stdout 2>> results/birth_analysis/weld.stderr 
+    python birth_analysis_grizzly.py -t $nthreads -f $SA_HOME/python/benchmarks/datasets/birth_analysis/_data/babynames.txt >> results/birth_analysis/weld.stdout 2>> results/birth_analysis/weld.stderr 
     python blackscholes_weld.py -t $nthreads -s 30 >> results/blackscholes/weld.stdout 2>> results/blackscholes/weld.stderr 
     python crime_index_grizzly.py -t $nthreads -s 30 >> results/crime_index/weld.stdout 2>> results/crime_index/weld.stderr 
     python data_cleaning_grizzly.py -t $nthreads -s 29 >> results/data_cleaning/weld.stdout 2>> results/data_cleaning/weld.stderr 
     python haversine_weld.py -t $nthreads -s 30 >> results/haversine/weld.stdout 2>> results/haversine/weld.stderr 
-    python movielens_grizzly.py -t $nthreads -f $HOME/composer/python/benchmarks/datasets/movielens/_data/ml-1m/ >> results/movielens/weld.stdout 2>> results/movielens/weld.stderr 
+    python movielens_grizzly.py -t $nthreads -f $SA_HOME/python/benchmarks/datasets/movielens/_data/ml-1m/ >> results/movielens/weld.stdout 2>> results/movielens/weld.stderr 
     # NOTE: These results should be run five times and summed, due to an OOM error in Weld.
     python nbody_weld.py -t $nthreads -s 15 -i 1 >> results/nbody/weld.stdout 2>> results/nbody/weld.stderr 
     # NOTE: These results should be run ten times and summed, due to an OOM error in Weld.
